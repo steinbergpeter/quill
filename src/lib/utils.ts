66f8454f8c2edb20310c905e7d92bf382a0a785c
Peter Steinberg <bklynpeter@gmail.com>
2023-09-29 10:08:08 -0400

    fix pnpm issue

---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from 'clsx';
+
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export default cn;
