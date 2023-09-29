fccd3d4b20304e27b153eb01ef2d5d21e38ea538
Peter Steinberg <bklynpeter@gmail.com>
2023-09-29 18:11:18 -0400

    added KindeAuth; getting 404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <>
-      <MaxWidthWrapper className='mb-12 mt-8 sm:mt-40 flex flex-col items-center justify-center text-center'>
+    <main>
+      <MaxWidthWrapper className='mb-12 mt-8 sm:mt-16 flex flex-col items-center justify-center text-center'>
         <div className='mx-auto mb-12 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
           <p className='text-sm font-semibold text-gray-700'>
             Quill is now public!
@@ -159,6 +159,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
